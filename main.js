fc886c4573b859c8310ef0139b410dc71e5ae3a8
potato w <potatow@potatodeMacBook-Pro.local>
2019-02-19 17:36:03 +0800

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+import validCode from './components/validCode.vue'
+
+Vue.component('validcode',validCode)
+
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
---
+++ pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+		<view>验证码：</view>
+		<validcode :maxlength="4" :isPwd="false" @finish="getCode"></validcode>
+		<view>密码:</view>
+		<validcode :maxlength="6" :isPwd="true" @finish="getPwd"></validcode>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	onLoad() {},
+	methods: {
+		getCode(val) {
+			console.log(val);
+		},
+		getPwd(val){
+			console.log(val);
+		}
+	}
+};
+</script>
+
+<style>
+.content {
+	height: 400upx;
+}
+</style>
